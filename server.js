304641871879f73948ab5aef85d161dabaf1f7b6
bisht-sonali <bishtsonali554@gmail.com>
2025-11-20 21:15:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,57 +1,54 @@
 const express = require("express");
 const multer = require("multer");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const cors = require("cors");
 const path = require("path");
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
+// Make uploads folder public
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-//mul storage config
-
-const storage = multer.disk.storage({
-    destination:(reg,file,cb) => {
-        cb(null,"uploads/");
+// Multer storage config
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/");
     },
-    filename:(reg,file,cb) =>{
-        cb(null,Date.now()+".webm");
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + ".webm");
     }
 });
 
-const upload = mutler({storage});
+const upload = multer({ storage });
 
-
-//MySql connection
-
+// MySQL connection
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"kyc_db"
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "kyc_db"
 });
 
-
-//API to upload vid
-
-app.post("/upload",upload.single("file"),(req,res) =>{
+// API to upload video
+app.post("/upload", upload.single("file"), (req, res) => {
     const name = req.body.name;
     const requestId = req.body.requestId;
-    const videoPath = req.body.path;
+    const videoPath = "/uploads/" + req.file.filename;
 
-    const sql = "INSERT INTO kyc_videos(customer_name, request_id, video_path)VALUES(?,?,?)";
-    db.query(sql[name,requestId,videoPath],(err) => {
-        if (err){
+    const sql = "INSERT INTO kyc_videos(customer_name, request_id, video_path) VALUES (?, ?, ?)";
+
+    db.query(sql, [name, requestId, videoPath], (err) => {
+        if (err) {
             console.log(err);
             return res.status(500).send("Database error");
         }
-        res.rend("upload succesful!");
+        res.send("Upload successful!");
     });
-}); 
-
-app.listen(3000,() =>{
-    console.log("Backend running at http.//localhost:300")
 });
 
-
-
+// Start server
+app.listen(3000, () => {
+    console.log("Backend running at http://localhost:3000");
+});
